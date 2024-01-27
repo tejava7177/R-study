a4111a7648e81dd88def5eb260779c194b02a08b
tejava7177 <wngmstla@sch.ac.kr>
2024-01-27 23:12:53 +0900

    5강
    변수, 벡터, 데이터프레임 및 리파지토리 생성

---
+++ 변수,벡터,데이터프레임.R
@@ -0,0 +1,14 @@
+shim / male / 35 / 180 / 70
+kim / female / 27 / 165 / 55
+osaki / female / 22 / 150 / 34
+choi / male / 40 / 175 / 80 
+
+gender = c('male', 'female', 'female', 'male')
+age = c(35,27,22,40)
+height = c(180,165,150,175)
+weight= c(70,55,34,80)
+
+shim = c("juheun", "male", 35 , 180, 75)
+
+
+mydf = data.frame(name, gender, age, height, weight)
