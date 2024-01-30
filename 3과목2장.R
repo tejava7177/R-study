b8ee4b8ff957a835499ab7321090596766cc6bab
tejava7177 <wngmstla@sch.ac.kr>
2024-01-30 15:14:59 +0900

    3과목 2장
    연습문제 : p.248~255

---
+++ 3과목2장.R
@@ -0,0 +1,19 @@
+x<-c(1:5)
+y<-seq(10,50,10)
+xy<-rbind(x,y)
+
+
+mx = matrix(c(1,2,3,4,5,6), ncol=2, byrow=T)
+mx = matrix(c(1,2,3,4,5,6), ncol=3, byrow=T)
+
+as.Date('08/23/2019', '%m/%d/%y')
+
+A <- cbind( c(1,2,3), c(4,5,6), c(7,8,9))
+colnames(A)<-c("A", "B", "C")
+rownames(A)<-c("r1", "r2", "r3")
+
+
+b<-c(1:4, 1:4)
+c<-rep(1:4, 2)
+d<-rep(1:4, time=2)
+e<-rep(1:4, each=2)
