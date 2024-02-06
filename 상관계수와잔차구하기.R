93670c1aab415694702c7feccae449b71d0d3c01
tejava7177 <wngmstla@sch.ac.kr>
2024-02-06 22:11:42 +0900

    잔차계산하기

---
+++ 상관계수와잔차구하기.R
@@ -0,0 +1,7 @@
+summary(aov(weight~feed, chickwts))
+      Df Sum Sq Mean Sq F value Pr(>F)
+feed  5  231129 46226 15.37 5.94e-10 ***
+Residuals 65  195556 3009
+---
+Signif. codes: 0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+  
